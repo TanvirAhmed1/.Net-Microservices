7af495b9a1b8cce5c9803ab80f13596bec96dfaa
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2022-11-10 19:00:07 +0600

    AutomapperProfile and Controller Added

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+using System;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScop = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScop.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding Data");
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "free" }
+                );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("Data already exists");
+            }
+        }
+    }
+}
--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PlatformService.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,8 +28,10 @@ namespace PlatformService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
@@ -54,6 +58,7 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+            PrepDb.PrepPopulation(app);
         }
     }
 }
